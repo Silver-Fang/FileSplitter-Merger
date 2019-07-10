3da0f0ef81771217d88fbfd0cb42706b6c6ba44d
锹甲阿尔法 <zhangtianfu0@126.com>
2019-07-10 15:06:19 +0800

    添加项目文件。

---
+++ 大文件拆分器/文件名比较器.vb
@@ -0,0 +1,9 @@
+﻿Imports Windows.Storage
+
+Public Class 文件名比较器
+	Implements IComparer(Of StorageFile)
+
+	Public Function Compare(x As StorageFile, y As StorageFile) As Integer Implements IComparer(Of StorageFile).Compare
+		Return SByte.Parse(x.Name.Split(".").Last) - SByte.Parse(y.Name.Split(".").Last)
+	End Function
+End Class
